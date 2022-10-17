ebf3c2cdec58131388ff3595db57dea662653a2e
leon <leon.fromont@gmail.com>
2022-10-18 09:24:50 +1300

    yay all files are now sym links

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-ln -f --symbolic ./keybindings.json ~/.config/Code/User/keybindings.json
-ln -f --symbolic ./.zshrc ~/.zshrc
+#!/bin/bash
+ln -f --symbolic $(pwd)/keybindings.json ~/.config/Code/User/keybindings.json
+ln -f --symbolic $(pwd)/settings.json ~/.config/Code/User/settings.json
+ln -f --symbolic $(pwd)/.zshrc ~/.zshrc
+# hosts can't be sym linked
 sudo cp ./hosts /etc/hosts
