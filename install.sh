2fab962f76256775897af527c59d44df6f616f04
leonfromont <leon.fromont@gmail.com>
2023-01-04 13:28:47 +1300

    Wed 04 Jan 2023 13:28:47 NZDT

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 ln -f --symbolic $(pwd)/keybindings.json ~/.config/Code/User/keybindings.json
 ln -f --symbolic $(pwd)/settings.json ~/.config/Code/User/settings.json
 ln -f --symbolic $(pwd)/.zshrc ~/.zshrc
+ln -f --symbolic $(pwd)/default.xml ~/.config/inkscape/keys
 # hosts can't be sym linked
 sudo cp ./hosts /etc/hosts
