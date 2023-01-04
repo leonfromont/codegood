2a054aa55462eea3787e4bd43b50b059f344dab0
leonfromont <leon.fromont@gmail.com>
2023-01-04 19:10:20 +1300

    sym link vscode stuff as well

--- install.ps1
+++ install.ps1
@@ -1,1 +1,3 @@
-New-Item -ItemType SymbolicLink -Target ./Microsoft.Powershell_profile.ps1 -Path $env:USERPROFILE\Documents\WindowsPowershell\Microsoft.PowerShell_profile.ps1
+New-Item -Force -ItemType SymbolicLink -Target ./Microsoft.Powershell_profile.ps1 -Path $env:USERPROFILE\Documents\WindowsPowershell\Microsoft.PowerShell_profile.ps1
+New-Item -Force -ItemType SymbolicLink -Target ./settings.json -Path $env:APPDATA\Code\User\settings.json
+New-Item -Force -ItemType SymbolicLink -Target ./keybindings.json -Path $env:APPDATA\Code\User\keybindings.json
